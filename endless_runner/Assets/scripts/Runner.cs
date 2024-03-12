5b0e47596f83d465ecbb5bbfabdfdd7edd7d3a30
tjm2285 <tjm2285@yahoo.com>
2024-03-12 15:34:50 -0700

    add basic runner script

---
+++ endless_runner/Assets/scripts/Runner.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class Runner : MonoBehaviour
+{
+    [SerializeField]
+    Light pointLight;
+
+    [SerializeField]
+    ParticleSystem explosionSystem, trailSystem;
+    [SerializeField, Min(0f)]
+    float startSpeedX = 5f;
+
+    MeshRenderer meshRenderer;
+
+    Vector2 position;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
+        pointLight.enabled = false;
+    }
+
+    public void StartNewGame()
+    {
+        position = Vector2.zero;
+        transform.localPosition = position;
+        meshRenderer.enabled = true;
+        pointLight.enabled = true;
+        explosionSystem.Clear();
+        SetTrailEmission(true);
+        trailSystem.Clear();
+        trailSystem.Play();
+    }
+
+    void Explode()
+    {
+        meshRenderer.enabled = false;
+        pointLight.enabled = false;
+        SetTrailEmission(false);
+        transform.localPosition = position;
+        explosionSystem.Emit(explosionSystem.main.maxParticles);
+    }
+    void SetTrailEmission(bool enabled)
+    {
+        ParticleSystem.EmissionModule emission = trailSystem.emission;
+        emission.enabled = enabled;
+    }
+
+    public bool Run(float dt)
+    {
+        if (position.x > 25f)
+        {
+            Explode();
+            return false;
+        }
+
+        position.x += startSpeedX * dt;
+        return true;
+    }
+    
+    public void UpdateVisualization()
+    {
+        transform.localPosition = position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
