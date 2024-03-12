4047125d68e3c87a2e43a6fd442ca9fe73fa814c
tjm2285 <tjm2285@yahoo.com>
2024-03-12 16:55:19 -0700

    added camera function to track width of skyline

--- endless_runner/Assets/scripts/TrackingCamera.cs
+++ endless_runner/Assets/scripts/TrackingCamera.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TrackingCamera : MonoBehaviour
 {
     Vector3 offset, position;
+
+    float viewFactorX;
     void Awake()
     {        
         offset = transform.localPosition;
-        
+        Camera camera = GetComponent<Camera>();
+        float viewFactorY = Mathf.Tan(camera.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        viewFactorX = viewFactorY  * camera.aspect;
     }
 
     public void StartNewGame()
@@ -21,4 +26,8 @@ public class TrackingCamera : MonoBehaviour
         position = focusPoint + offset;
         transform.localPosition = position;
     }
+
+    //returns a range made using the camera's X position and the view factor X scaled by the relative Z distance as its extents
+    public FloatRange VisibleX(float z) =>
+        FloatRange.PositionExtents(position.x, viewFactorX * (z - position.z));
 }
