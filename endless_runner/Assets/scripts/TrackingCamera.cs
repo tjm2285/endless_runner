36938acfd1f8778d85287a126e40ada4fa3abdd5
tjm2285 <tjm2285@yahoo.com>
2024-03-13 09:50:15 -0700

    added star particles to camera

--- endless_runner/Assets/scripts/TrackingCamera.cs
+++ endless_runner/Assets/scripts/TrackingCamera.cs
@@ -8,17 +8,27 @@ public class TrackingCamera : MonoBehaviour
     Vector3 offset, position;
 
     float viewFactorX;
+    ParticleSystem stars;
     void Awake()
     {        
         offset = transform.localPosition;
         Camera camera = GetComponent<Camera>();
         float viewFactorY = Mathf.Tan(camera.fieldOfView * 0.5f * Mathf.Deg2Rad);
         viewFactorX = viewFactorY  * camera.aspect;
+
+        stars = GetComponent<ParticleSystem>();
+        ParticleSystem.ShapeModule shape = stars.shape;
+        Vector3 position = shape.position;
+        position.y = viewFactorY * position.z * 0.5f;
+        shape.position = position;
+        shape.scale = new Vector3(2f * viewFactorX, viewFactorY) * position.z;
     }
 
     public void StartNewGame()
     {
-        Track(Vector3.zero);   
+        Track(Vector3.zero);
+        stars.Clear();
+        stars.Emit(stars.main.maxParticles);
     }
 
     public void Track(Vector3 focusPoint)
